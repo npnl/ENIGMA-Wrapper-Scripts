4c024b08a643f9a8297a6aa050a23dbca1415c42
npnl <npnlusc@gmail.com>
2015-12-18 15:33:39 -0800

    deleted

--- enigma_wrapscripts/Matlab/create_pngs_cortical.m
+++
@@ -1,7 +0,0 @@
-addpath(getenv('mdir'))
-QC_output_directory=getenv('qcdir');
-FS_directory=getenv('fsdir');
-b=getenv('sub');
-
-func_make_corticalpngs_ENIGMA_QC(QC_output_directory,b, [FS_directory,'/', b, '/mri/orig.mgz'], [FS_directory,'/',b, '/mri/aparc+aseg.mgz']);
-
--- enigma_wrapscripts/bash/recon-all.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-export FREESURFER_HOME=<PATH TO FREESURFER INSTALLATION>
-source ${FREESURFER_HOME}/SetUpFreeSurfer.sh
-export PATH=${PATH}:${FREESURFER_HOME}/bin
-
-recon-all $@
-
-exit $?
